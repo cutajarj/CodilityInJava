b58a4eaa893a1a0b03c6e8b25fdecf5352a4702e
cutajarj2 <james.cutajar@blip.pt>
2019-02-05 20:38:10 +0000

    Number of passing cars docs

--- src/main/java/com/cutajarjames/codility/prefixsums/PassingCars.java
+++ src/main/java/com/cutajarjames/codility/prefixsums/PassingCars.java
@@ -1,14 +1,18 @@
 package com.cutajarjames.codility.prefixsums;
 
+/**
+ * This is the solution for Prefix Sums > Passing Cars
+ * <p>
+ * This is marked as PAINLESS difficulty
+ */
+
 public class PassingCars {
+
     public int solution(int[] A) {
         int[] suffixSum = new int[A.length + 1];
+        int count = 0;
         for (int i = A.length - 1; i >= 0; i--) {
             suffixSum[i] = A[i] + suffixSum[i + 1];
-        }
-
-        int count = 0;
-        for (int i = 0; i < A.length; i++) {
             if (A[i] == 0) count += suffixSum[i];
             if (count > 1000000000) return -1;
         }
