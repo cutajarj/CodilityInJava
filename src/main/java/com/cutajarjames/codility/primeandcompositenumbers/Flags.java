fa74bbb29f636f54c8a5dc5209536175fcc7013c
cutajarj2 <james.cutajar@blip.pt>
2019-04-21 17:28:25 +0100

    fixed small bug

--- src/main/java/com/cutajarjames/codility/primeandcompositenumbers/Flags.java
+++ src/main/java/com/cutajarjames/codility/primeandcompositenumbers/Flags.java
@@ -33,6 +33,6 @@ public class Flags {
     }
 
     public static void main(String[] args) {
-        System.out.println(new FlagsFaster().solution(new int[]{1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2}));
+        System.out.println(new Flags().solution(new int[]{1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2}));
     }
 }
