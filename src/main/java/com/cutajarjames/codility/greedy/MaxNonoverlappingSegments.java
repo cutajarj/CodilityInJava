3473cc5c1484873064939fc83e6f41c6ea8fe507
cutajarj <james.cutajar@gmail.com>
2020-03-12 09:53:23 +0000

    Add the non overlapping segments problem

---
+++ src/main/java/com/cutajarjames/codility/greedy/MaxNonoverlappingSegments.java
@@ -0,0 +1,28 @@
+package com.cutajarjames.codility.greedy;
+
+/**
+ * This is the solution for Greedy algorithms > MaxNonoverlappingSegments
+ * The problem is equivalent to the Activity Selection Problem,
+ * where you have to choose the maximum non overlapping tasks.
+ * <p>
+ * This is marked as PAINLESS difficulty
+ */
+
+public class MaxNonoverlappingSegments {
+    public int solution(int[] A, int[] B) {
+        int lastEndSegment = -1;
+        int chosenCount = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > lastEndSegment) {
+                chosenCount++;
+                lastEndSegment = B[i];
+            }
+        }
+        return chosenCount;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MaxNonoverlappingSegments().solution(new int[]{1, 3, 7, 9, 9}, new int[]{5, 6, 8, 9, 10}));
+    }
+
+}
