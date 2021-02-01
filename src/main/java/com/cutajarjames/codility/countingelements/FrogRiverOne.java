a70bb33a2718ca2d9d912ed9cf538abba9e1e9e6
cutajarj <james.cutajar@gmail.com>
2021-02-01 17:19:04 +0000

    Adding solution for FrogRiverOne

---
+++ src/main/java/com/cutajarjames/codility/countingelements/FrogRiverOne.java
@@ -0,0 +1,22 @@
+package com.cutajarjames.codility.countingelements;
+
+public class FrogRiverOne {
+    public int solution(int X, int[] A) {
+        boolean[] riverPositions = new boolean[X + 1];
+        for (int time = 0; time < A.length; time++) {
+            int pos = A[time];
+            if (!riverPositions[pos]) {
+                riverPositions[pos] = true;
+                X -= 1;
+                if (X == 0) return time;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FrogRiverOne().solution(5, new int[]{1, 3, 1, 4, 2, 3, 5, 4}));
+        System.out.println(new FrogRiverOne().solution(1, new int[]{1, 1, 1}));
+        System.out.println(new FrogRiverOne().solution(3, new int[]{1, 2, 1}));
+    }
+}
